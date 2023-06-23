55b9a7422540994ee5e5c3463f7a38035805f999
vamsiKrishna-511 <116443206+vamsiKrishna-511@users.noreply.github.com>
2023-06-23 22:51:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var button = document.querySelector('#start');
+            var container = document.querySelector('#jitsi-container');
+            var api = null;
+
+            button.addEventListener('click', () => {
+                var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                var stringLength = 30;
+                function pickRandom () {
+                    return possible[Math.floor() * possible.length];
+                }
+                var randomString = Array.apply(null,Array(stringLength)).map (pickRandom).join('');
+
+                var domain = "meet.jit.si";
+                var options = {
+                    "roomName": randomString,
+                    "parentNode": container,
+                    "width": 600,
+                    "height": 600,
+                };
+                api = new JitsiMeetExternalAPI(domain, options);
+             });
